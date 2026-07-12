8461d5dcef44661afa574a84d2a287a76eca1736
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-12 18:08:24 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,11 +3,11 @@
 
 # sudo apt-get update
 # sudo apt-get install -y cuda-toolkit-13-0
-cd gpu_burn
-make CUDAPATH=/usr/local/cuda-13.0
-./gpu_burn 36000000000
+# cd gpu_burn
+# make CUDAPATH=/usr/local/cuda-13.0
+# ./gpu_burn 36000000000
 # screen -ls
-# nvidia-smi
+nvidia-smi
 
 # pkill -f gpu_burn 2>/dev/null || true
 # nvidia-smi
