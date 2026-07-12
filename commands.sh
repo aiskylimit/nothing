301fab29724eb58a252fbc148123f7132eba68c6
phongdq25 <phongdq25@gmail.com>
2026-07-12 15:32:41 +0700

    init

---
+++ commands.sh
@@ -0,0 +1,13 @@
+#0
+#gpu-burn-log
+
+# sudo apt-get update
+# sudo apt-get install -y cuda-toolkit-13-0
+# make CUDAPATH=/usr/local/cuda-13.0
+# ./gpu_burn 36000000000
+# screen -ls
+# nvidia-smi
+
+# pkill -f gpu_burn 2>/dev/null || true
+# nvidia-smi
+#3
