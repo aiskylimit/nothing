d7900ff61cc7cb44c615e5861118ed57ef8b1df3
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-13 09:15:55 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,44 +1,50 @@
-#2
+#1 +30
 #main
 
 # sudo apt-get update
 # sudo apt-get install -y cuda-toolkit-13-0
-# bash install_miniconda.sh
+bash install_miniconda.sh
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda activate base
+
+# which python
+# which pip
+
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
 # screen -ls
-#nvidia-smi
+# nvidia-smi
 
-pkill -f gpu_burn 2>/dev/null || true
-nvidia-smi
-#git clone --branch new_ap https://github.com/DanhVinhLe/Rebuttal_VLM2Vec_Matryoshka.git
-cd Rebuttal_VLM2Vec_Matryoshka
-apt-get update
-apt-get upgrade -y
-apt install tmux zip unzip -y
-apt-get install -y libgl1 libglib2.0-0
-rm -rf ./vlm
-python -m venv vlm 
-source vlm/bin/activate
-pip install -r requirements.txt
-python download.py
-wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
-unzip images.zip -d eval_images/
-python fix_lib.py
-bash script_train/fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash script_train/fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
-bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
-bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
-bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
-bash eval.sh
-bash eval2.sh
-bash eval5.sh
-bash eval6.sh
-bash eval_vqa.sh
-bash eval_vqa2.sh
-bash eval_vqa3.sh
-bash eval_vqa4.sh
+# pkill -f gpu_burn 2>/dev/null || true
+# nvidia-smi
+# git clone --branch new_ap https://github.com/DanhVinhLe/Rebuttal_VLM2Vec_Matryoshka.git
+# cd Rebuttal_VLM2Vec_Matryoshka
+# apt-get update
+# apt-get upgrade -y
+# apt install tmux zip unzip -y
+# apt-get install -y libgl1 libglib2.0-0
+# rm -rf ./vlm
+# python -m venv vlm 
+# source vlm/bin/activate
+# pip install -r requirements.txt
+# python download.py
+# wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
+# unzip images.zip -d eval_images/
+# python fix_lib.py
+# bash script_train/fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash script_train/fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
+# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
+# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash eval.sh
+# bash eval2.sh
+# bash eval5.sh
+# bash eval6.sh
+# bash eval_vqa.sh
+# bash eval_vqa2.sh
+# bash eval_vqa3.sh
+# bash eval_vqa4.sh
 
 #9
