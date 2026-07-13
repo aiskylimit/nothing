1cfd6f905675f8b40a72785ef8f9b87964fa4210
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-13 11:19:00 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,10 @@
-#1 +10
+#1 +300
 #main
 
-# sudo apt-get update
-# sudo apt-get install -y cuda-toolkit-13-0
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt-get install -y cuda-toolkit-13-0
 # bash install_miniconda.sh
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
