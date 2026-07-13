bdea10cac008d52e6856cbf9e179151970c3973a
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-13 12:51:46 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +600
+#1 +10
 #main
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
@@ -19,42 +19,43 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # ./gpu_burn 36000000000
 
 # screen -ls
-# nvidia-smi
+nvidia-smi
 
 # pkill -f gpu_burn 2>/dev/null || true
 # nvidia-smi
 # git clone --branch new_ap https://github.com/DanhVinhLe/Rebuttal_VLM2Vec_Matryoshka.git
 cd Rebuttal_VLM2Vec_Matryoshka
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt install tmux zip unzip -y
-sudo apt-get install -y libgl1 libglib2.0-0
+ls
+# sudo apt-get update
+# sudo apt-get upgrade -y
+# sudo apt install tmux zip unzip -y
+# sudo apt-get install -y libgl1 libglib2.0-0
 
-conda create -n python311 python=3.11 -y
-conda activate python311
+# conda create -n python311 python=3.11 -y
+# conda activate python311
 
-rm -rf ./vlm
-python -m venv vlm 
-source vlm/bin/activate
-pip install -r requirements.txt
-python download.py
+# rm -rf ./vlm
+# python -m venv vlm 
+# source vlm/bin/activate
+# pip install -r requirements.txt
+# python download.py
 
 # wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
 # unzip images.zip -d eval_images/
-python fix_lib.py
-bash script_train/fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash script_train/fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
-bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
-bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
-bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
-bash eval.sh
-bash eval2.sh
-bash eval5.sh
-bash eval6.sh
-bash eval_vqa.sh
-bash eval_vqa2.sh
-bash eval_vqa3.sh
-bash eval_vqa4.sh
+# python fix_lib.py
+# bash script_train/fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash script_train/fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
+# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
+# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash eval.sh
+# bash eval2.sh
+# bash eval5.sh
+# bash eval6.sh
+# bash eval_vqa.sh
+# bash eval_vqa2.sh
+# bash eval_vqa3.sh
+# bash eval_vqa4.sh
 
 #9
