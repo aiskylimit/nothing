71f0b58a969cfcd1dcfbf6b0c7e3d93045b9e91c
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-13 08:14:48 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,46 +1,46 @@
-#1
+#2
 #main
 
 # sudo apt-get update
 # sudo apt-get install -y cuda-toolkit-13-0
 # bash install_miniconda.sh
 # make CUDAPATH=/usr/local/cuda-13.0
-./gpu_burn 36000000000
+# ./gpu_burn 36000000000
 
 # screen -ls
 #nvidia-smi
 
-pkill -f gpu_burn 2>/dev/null || true
-nvidia-smi
-git clone --branch new_ap https://github.com/DanhVinhLe/Rebuttal_VLM2Vec_Matryoshka.git
-cd Rebuttal_VLM2Vec_Matryoshka
-cd Rebuttal_VLM2Vec_Matryoshka
-apt-get update
-apt-get upgrade -y
-apt install tmux zip unzip -y
-apt-get install -y libgl1 libglib2.0-0
-python -m venv vlm 
-source vlm/bin/activate
-pip install -r requirements.txt
-python download.py
-wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
-unzip images.zip -d eval_images/
-python fix_lib.py
-bash script_train/fastvlm_adaptive_mrl_stage1_cls_0.5_0.1.sh
-bash script_train/fastvlm_adaptive_mrl_stage1_cls_0.5_0.3.sh
-bash fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
-bash qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
-bash qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
-bash qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
-bash qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
-bash eval.sh
-bash eval2.sh
-bash eval5.sh
-bash eval6.sh
-bash eval_vqa.sh
-bash eval_vqa2.sh
-bash eval_vqa3.sh
-bash eval_vqa4.sh
+# pkill -f gpu_burn 2>/dev/null || true
+# nvidia-smi
+# git clone --branch new_ap https://github.com/DanhVinhLe/Rebuttal_VLM2Vec_Matryoshka.git
+# cd Rebuttal_VLM2Vec_Matryoshka
+# cd Rebuttal_VLM2Vec_Matryoshka
+# apt-get update
+# apt-get upgrade -y
+# apt install tmux zip unzip -y
+# apt-get install -y libgl1 libglib2.0-0
+# python -m venv vlm 
+# source vlm/bin/activate
+# pip install -r requirements.txt
+# python download.py
+# wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
+# unzip images.zip -d eval_images/
+# python fix_lib.py
+# bash script_train/fastvlm_adaptive_mrl_stage1_cls_0.5_0.1.sh
+# bash script_train/fastvlm_adaptive_mrl_stage1_cls_0.5_0.3.sh
+# bash fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
+# bash qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
+# bash qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
+# bash qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
+# bash eval.sh
+# bash eval2.sh
+# bash eval5.sh
+# bash eval6.sh
+# bash eval_vqa.sh
+# bash eval_vqa2.sh
+# bash eval_vqa3.sh
+# bash eval_vqa4.sh
 
 #9
