354f0dd4f677d44715b512006cbfa91516df635c
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-13 13:25:59 -0500

    check log

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +10
+#2
 #main
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
@@ -9,10 +9,10 @@
 # echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 # source ~/.bashrc
 # bash install_miniconda.sh
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate base
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+#source ~/miniconda3/etc/profile.d/conda.sh
+#conda activate base
+#export PATH=/usr/local/cuda/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
 # make CUDAPATH=/usr/local/cuda-13.0
@@ -22,7 +22,7 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # nvidia-smi
 
 # pkill -f gpu_burn 2>/dev/null || true
-nvidia-smi
+#nvidia-smi
 
 # conda create -n python311 python=3.11 -y
 # conda activate python311
