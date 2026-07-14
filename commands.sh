21aaa6ab13c00a47e5822fe3c4544bb236e5b37c
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-14 08:34:20 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +5
+#2 -f-Rebuttal_VLM2Vec_Matryoshka/MMEB-evaloutputs/AdaptiveMRL_qwen3_vqa_projection_only/ +a
 #main
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
