88be206ae51dbf88b3912ef36aef3f9f9368c4b3
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-14 08:40:47 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #1 +5
 #main
-#2 -f-Rebuttal_VLM2Vec_Matryoshka/MMEB-evaloutputs/AdaptiveMRL_qwen3_vqa_projection_only/ +a
+#2 -f-~/Rebuttal_VLM2Vec_Matryoshka/MMEB-evaloutputs/AdaptiveMRL_qwen3_vqa_projection_only/ +a
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
