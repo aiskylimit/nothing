b52609bdf006f65c90a2429118b4d9e5b5c321f7
phongdq25 <nothing>
2026-07-15 14:54:27 +0700

    ai-tutor

--- commands.sh
+++ commands.sh
@@ -1,7 +1,6 @@
-#1 +30
+#1 +600
 #main
 
-#2 -f-/home/ubuntu/aiskylimit_nothing/ +a
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
@@ -12,53 +11,21 @@
 # source ~/.bashrc
 # bash install_miniconda.sh
 
-cd gpu_burn
-make CUDAPATH=/usr/local/cuda-13.0
-./gpu_burn 36000000000
+# cd gpu_burn
+# make CUDAPATH=/usr/local/cuda-13.0
+# ./gpu_burn 36000000000
 
-# source ~/miniconda3/etc/profile.d/conda.sh
-# conda activate base
-# export PATH=/usr/local/cuda/bin:$PATH
-# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+pkill -f gpu_burn 2>/dev/null || true
 
 
 
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate base
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-# screen -ls
-# nvidia-smi
 
-# pkill -f gpu_burn 2>/dev/null || true
-#nvidia-smi
 
-# conda create -n python311 python=3.11 -y
-# conda activate python311
-
-# cd Rebuttal_VLM2Vec_Matryoshka
-# python ./copy_res.py
-# ls
-
-# rm -rf ./vlm
-# python -m venv vlm 
-# source vlm/bin/activate
-# pip install -r requirements.txt
-# python download.py
-
-# wget https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip
-# unzip images.zip -d eval_images/
-# python fix_lib.py
-# bash script_train/fastvlm_adaptive_mrl_laplacian_only_stage1_vqa.sh
-# bash script_train/fastvlm_adaptive_mrl_projection_only_stage1_vqa.sh
-# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_cls.sh
-# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_cls.sh
-# bash script_train/qwen3vl_adaptive_mrl_laplacian_only_stage1_vqa.sh
-# bash script_train/qwen3vl_adaptive_mrl_projection_only_stage1_vqa.sh
-# bash eval.sh
-# bash eval2.sh
-# bash eval5.sh
-# bash eval6.sh
-# bash eval_vqa.sh
-# bash eval_vqa2.sh
-# bash eval_vqa3.sh
-# bash eval_vqa4.sh
-
-#9
+cd ai-tutor-training
+bash install.sh
+bash project_commands.sh
