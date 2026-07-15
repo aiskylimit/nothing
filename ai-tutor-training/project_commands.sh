9c4fb7b9b4b73722cdb3bc02a46b24f87e9b0b22
phongdq25 <nothing>
2026-07-15 16:27:24 +0700

    ai-tutor

--- ai-tutor-training/project_commands.sh
+++ ai-tutor-training/project_commands.sh
@@ -1,3 +1,5 @@
 source .venv/bin/activate
 
+hf download Dream-AI-HUST/test --repo-type dataset --local-dir data
+
 bash ./start_rl_training.sh --config_file config/deepspeed/zero2_4GPU.yaml --config-name qwen3.5_9b.yaml
