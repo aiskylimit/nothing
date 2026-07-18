ebb7a6d36886d80a2ab9fd657b1b6fc47549c4dc
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-19 00:03:35 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
 #sql-main
-#v2
+#v3
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
 nvidia-smi
