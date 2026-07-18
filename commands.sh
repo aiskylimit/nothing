15816bb6b76f715b67783910b6502acde40d43af
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-18 23:14:46 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
-#1 +300
+#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
 #sql-main
 #v2
 
-#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_150012/jobs/ +a
+#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
 nvidia-smi
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
@@ -30,5 +30,5 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-cd text2sql_distillation_draft
-bash ./project_commands.sh
+# cd text2sql_distillation_draft
+# bash ./project_commands.sh
