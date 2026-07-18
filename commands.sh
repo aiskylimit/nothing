b5be91db43c0c918942a1d50e2d165dee9ccb27b
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-18 14:31:55 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,8 @@
-#1 +10
+#1 +300
 #sql-main-log
 #v1
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_065707/jobs/ +a
-# kill -9 $(pgrep -f "_draft/.venv/bin/python")
 nvidia-smi
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
@@ -21,13 +20,13 @@ nvidia-smi
 
 # pkill -f gpu_burn 2>/dev/null || true
 
-# kill -9 $(pgrep -f "_draft/.venv/bin/python")
+# kill -9 $(pgrep -f "/.venv/bin/python")
 
-# source ~/miniconda3/etc/profile.d/conda.sh
-# conda activate base
-# export PATH=/usr/local/cuda/bin:$PATH
-# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate base
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-# cd text2sql_distillation_draft
-# bash ./project_commands.sh
+cd text2sql_distillation_draft
+bash ./project_commands.sh
