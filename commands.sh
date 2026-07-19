0410d32d96ded01b709e117a4c6852be008c9a53
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-19 23:11:49 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,10 +1,10 @@
-#1 +10
+#1 +30
 #sql-main
 #v3
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
 # nvidia-smi
-ls /home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/synid_ce_multilayer_3/qwen/spider_data/seed42
+# ls /home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/synid_ce_multilayer_3/qwen/spider_data/seed42
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
@@ -21,9 +21,9 @@ ls /home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/syni
 
 # pkill -f gpu_burn 2>/dev/null || true
 
-# kill -9 $(pgrep -f "/.venv/bin/python")
-# sleep 20
-# nvidia-smi
+kill -9 $(pgrep -f "/.venv/bin/python")
+sleep 20
+nvidia-smi
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
