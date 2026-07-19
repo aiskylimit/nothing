5fa495671f582eae38680d59e37dcca9c8d2c3e7
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-20 00:07:09 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +300
+#2
 #sql-main
 #v3
 
@@ -18,11 +18,10 @@
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
-# pkill -f gpu_burn 2>/dev/null || true
-
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
+
 # sleep 20
-nvidia-smi
+# nvidia-smi
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
@@ -30,8 +29,8 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-cd text2sql_distillation_draft
-bash ./project_commands.sh
+# cd text2sql_distillation_draft
+# bash ./project_commands.sh
 # source .venv/bin/activate
 # python -c "import nltk; nltk.download('punkt_tab')"
 # bash scripts/qwen/synid_ce_multilayer_3/format_eval_multiseed.sh
