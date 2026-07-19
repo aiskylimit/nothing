05c81f6a8e3cb82e6c7c797fa6666c956c882038
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-19 21:52:14 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,10 +1,9 @@
-#1 +10
+#1 +300
 #sql-main
 #v2
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260718_160742/jobs/ +a
-nvidia-smi
-ls /home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/infer/synid_ce_multilayer_3/qwen/spider_data/seed42
+# nvidia-smi
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
@@ -31,8 +30,8 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-# cd text2sql_distillation_draft
-# source .venv/bin/activate
-# python -c "import nltk; nltk.download('punkt_tab')"
-# bash scripts/qwen/synid_ce_multilayer_3/format_eval_multiseed.sh
+cd text2sql_distillation_draft
+source .venv/bin/activate
+python -c "import nltk; nltk.download('punkt_tab')"
+bash scripts/qwen/synid_ce_multilayer_3/format_eval_multiseed.sh
 # bash ./project_commands.sh
