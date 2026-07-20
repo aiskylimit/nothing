08d1b5839d06ee8bc0e81813ce6db7cd699053db
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-20 21:50:30 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -29,7 +29,7 @@ conda activate base
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-ls text2sql_distillation_draft/results/eval/synid_ce_no_keywords_weight/qwen -R
+ls text2sql_distillation_draft/results/eval -R
 # cd text2sql_distillation_draft
 # bash ./project_commands.sh
 # source .venv/bin/activate
