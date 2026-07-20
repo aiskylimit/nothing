c322628dba6e0f6b616179180877d5ea54002bff
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2026-07-20 21:33:07 +0700

    small update project_commands.sh

--- text2sql_distillation_draft/project_commands.sh
+++ text2sql_distillation_draft/project_commands.sh
@@ -17,44 +17,22 @@ unzip benchmarks.zip
 unzip data.zip
 # python ./scripts/synid_augment/build_teacher_train_from_final_merged.py
 
-RUNNER_GPU_LIST=0,1,2,3 \
-GPUS_PER_JOB=4 \
-RUN_MODE=parallel \
-SKIP_EXISTING=false \
-INFER_SEEDS=10,42,50,100,1234 \
-EVAL_BATCH_SIZE=32 \
-INFER_BATCH_SIZE=128 \
-bash scripts/qwen_updated/synid_ce_keywords_weight_lora_218/run_full_pipeline.sh &
+run_qwen_updated_pipeline() {
+  local pipeline_script="$1"
 
-RUNNER_GPU_LIST=4,5,6,7 \
-GPUS_PER_JOB=4 \
-RUN_MODE=parallel \
-SKIP_EXISTING=false \
-INFER_SEEDS=10,42,50,100,1234 \
-EVAL_BATCH_SIZE=32 \
-INFER_BATCH_SIZE=128 \
-bash scripts/qwen_updated/synid_ce_keywords_weight_lora_436/run_full_pipeline.sh &
+  RUNNER_GPU_LIST="${RUNNER_GPU_LIST:-0,1,2,3}" \
+  GPUS_PER_JOB="${GPUS_PER_JOB:-4}" \
+  RUN_MODE="${RUN_MODE:-parallel}" \
+  SKIP_EXISTING="${SKIP_EXISTING:-false}" \
+  INFER_SEEDS="${INFER_SEEDS:-10,42,50,100,1234}" \
+  EVAL_BATCH_SIZE="${EVAL_BATCH_SIZE:-32}" \
+  INFER_BATCH_SIZE="${INFER_BATCH_SIZE:-128}" \
+  bash "${pipeline_script}"
+}
 
-wait
-
-RUNNER_GPU_LIST=0,1,2,3 \
-GPUS_PER_JOB=4 \
-RUN_MODE=parallel \
-SKIP_EXISTING=false \
-INFER_SEEDS=10,42,50,100,1234 \
-EVAL_BATCH_SIZE=32 \
-INFER_BATCH_SIZE=128 \
-bash scripts/qwen_updated/synid_ce_no_keywords_weight_lora_218/run_full_pipeline.sh &
-
-RUNNER_GPU_LIST=4,5,6,7 \
-GPUS_PER_JOB=4 \
-RUN_MODE=parallel \
-SKIP_EXISTING=false \
-INFER_SEEDS=10,42,50,100,1234 \
-EVAL_BATCH_SIZE=32 \
-INFER_BATCH_SIZE=128 \
-bash scripts/qwen_updated/synid_ce_no_keywords_weight_lora_436/run_full_pipeline.sh &
-
-wait
+run_qwen_updated_pipeline scripts/qwen_updated/synid_ce_keywords_weight_lora_218/run_full_pipeline.sh
+run_qwen_updated_pipeline scripts/qwen_updated/synid_ce_keywords_weight_lora_436/run_full_pipeline.sh
+run_qwen_updated_pipeline scripts/qwen_updated/synid_ce_no_keywords_weight_lora_218/run_full_pipeline.sh
+run_qwen_updated_pipeline scripts/qwen_updated/synid_ce_no_keywords_weight_lora_436/run_full_pipeline.sh
 
 bash scripts/qwen_updated/upload_to_hf.sh
