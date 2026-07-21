efb82555ea5abd46f35fb7e9852182ce96b91d6c
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-21 16:21:51 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260721_083929/jobs/ +a
 #sql-main-log
-#v1
+#v2
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260721_083929/jobs/ +a
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/synid_ce_multilayer_3/qwen/collect/ +a
