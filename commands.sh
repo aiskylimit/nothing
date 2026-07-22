622c6b0fe25563083b36fef35a70d5f3b433ba56
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-22 09:31:25 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/home/ubuntu/aiskylimit_nothing/ark/results/ +a
+#1 +10
 #sql-main-log
 #v2
 
@@ -32,7 +32,7 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 # cd text2sql_distillation_draft
 # bash ./project_commands.sh
-# ls text2sql_distillation_draft/results/eval -R
+ls text2sql_distillation_draft/results/eval -R
 # source .venv/bin/activate
 # python -c "import nltk; nltk.download('punkt_tab')"
 # bash scripts/qwen/synid_ce_multilayer_3/format_eval_multiseed.sh
