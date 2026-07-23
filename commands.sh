31a19de2d829bb78a7ef0a142200d59146d22c38
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-23 09:55:42 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/eval_infer.zip +a
+#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval_infer.zip +a
 #sql-main
 #v2
 
