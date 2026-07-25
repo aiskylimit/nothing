41f8e866581dcaccdb9ddc57a14bc18079d86fe6
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-25 14:59:13 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/kid/both/20260725_071711/ +a
+#1 +10
 #sql-main
 #v2
 
@@ -33,12 +33,12 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 cd text2sql_distillation_draft
 
 # ls text2sql_distillation_draft/results -R
-source .venv/bin/activate
-MODEL_FAMILY=both LOG_TO_FILE=1 bash ./project_commands_kid.sh
+# source .venv/bin/activate
+# MODEL_FAMILY=both LOG_TO_FILE=1 bash ./project_commands_kid.sh
 # RUN_GPUS=0,1,2,3,4,5,6,7 CKPT_STEP=874 bash scripts/qwen_ablation_3/infer_csd_ckpt1090.sh
-cd ./results
-zip -r eval_infer.zip eval infer
-du -sh eval_infer.zip
+# cd ./results
+# zip -r eval_infer.zip eval infer
+# du -sh eval_infer.zip
 # bash ./eval.sh
 # python -c "import nltk; nltk.download('punkt_tab')"
 # bash scripts/qwen/synid_ce_multilayer_3/format_eval_multiseed.sh
