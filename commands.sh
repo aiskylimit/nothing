2ec583b3edff227a5b9c308ecca49c0a29f1af49
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-31 22:14:46 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,23 +1,24 @@
 #1 +10
-#sql-main
+#main
 #v2
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/run_logs/20260723_150003/jobs/ +a
 #2 -f-/home/ubuntu/aiskylimit_nothing/text2sql_distillation_draft/results/eval/synid_ce_keywords_weight_lora_436/qwen_updated/collect/ +a
 # nvidia-smi
 
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
-# sudo dpkg -i cuda-keyring_1.1-1_all.deb
-# sudo apt update
-# sudo apt-get install -y cuda-toolkit-13-0
-# echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
-# echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
-# source ~/.bashrc
-# bash install_miniconda.sh
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt-get install -y cuda-toolkit-13-0
+sudo apt install -y zip unzip
+echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+bash install_miniconda.sh
 
-# cd gpu_burn
-# make CUDAPATH=/usr/local/cuda-13.0
-# ./gpu_burn 36000000000
+cd gpu_burn
+make CUDAPATH=/usr/local/cuda-13.0
+./gpu_burn 36000000000
 
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
 
@@ -30,9 +31,9 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-cd text2sql_distillation_draft
+# cd text2sql_distillation_draft
 
-ls text2sql_distillation_draft/kid/KID-code/dbgpt_hub/output/adapter_kd/spider -R
+# ls text2sql_distillation_draft/kid/KID-code/dbgpt_hub/output/adapter_kd/spider -R
 # source .venv/bin/activate
 # MODEL_FAMILY=both LOG_TO_FILE=1 bash ./project_commands_kid.sh
 # RUN_GPUS=0,1,2,3,4,5,6,7 CKPT_STEP=874 bash scripts/qwen_ablation_3/infer_csd_ckpt1090.sh
