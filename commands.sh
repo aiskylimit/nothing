89c6c96808a75cec9d8b6c5dc095ba5ba5f3c6e2
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-08-04 13:40:38 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -0-3
+#1 +10
 #talas
 #v1
 
@@ -29,8 +29,10 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
-# cd Gen-DPO
-# CUDA_VISIBLE_DEVICES=6,7 bash ./project_commands.sh
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
+cd Gen-DPO
+rm -rf .venv-tis-dpo
+CUDA_VISIBLE_DEVICES=6,7 bash ./project_commands.sh
+
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
