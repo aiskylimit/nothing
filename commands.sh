99c010422df20e6577ffe90669e1b15434e9d1bd
phongdq <phongdq25@gmail.com>
2026-08-08 11:12:37 +0000

    pulll

--- commands.sh
+++ commands.sh
@@ -19,8 +19,8 @@
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
-# kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-# sleep 5
+kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
+sleep 5
 nvidia-smi
 
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -30,25 +30,22 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 
 
-cd Gen-DPO
-cd eval
-uv sync
-cd ..
-source eval/.venv/bin/activate
-CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/dpo_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_15-06
-CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/gendpo_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_21-05
-CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/sft_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_11-52
+# cd Gen-DPO
+# source eval/.venv/bin/activate
+# CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/dpo_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_15-06
+# CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/gendpo_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_21-05
+# CUDA_VISIBLE_DEVICES=6,7 bash all.sh output/sft_Llama-3.1-Tulu-3-8B-SFT_ultra-feedback_08-06_11-52
 # CUDA_VISIBLE_DEVICES=2,3 bash ./project_commands.sh
 
 # cd ../gpu_burn
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
-# cd ./talas_vlm_embed
+cd ./talas_vlm_embed
 # ls
 # ls training
 # ls training/FastVLM-0.5B_talas_1.0_eos_norm_proj_cls
-# bash ./project_commands.sh
+bash ./project_commands.sh
 
 # cd ./segd_distillation
 # bash ./project_commands.sh
