16dbf353645b87f91a1ff5cd59299cf34b0b95d4
phongdq <phongdq25@gmail.com>
2026-08-13 04:18:55 +0000

    upd

--- talas_vlm_embed/scripts/train_centroid_cls.sh
+++ talas_vlm_embed/scripts/train_centroid_cls.sh
@@ -19,7 +19,7 @@ torchrun --standalone \
     --percent_data 1.0 \
     --image_resolution "mid" \
     --output_dir "training/B3_Qwen2_2B_centroid_cls" \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 24 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-4 \
     --weight_decay 0.01 \
