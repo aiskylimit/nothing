377b736a2ef48ca3c047fb5bb6d320c6a6cc38c2
phongdq25 <nothing>
2026-08-13 13:56:49 +0700

    update

--- reward-guidance-main/project_command.sh
+++ reward-guidance-main/project_command.sh
@@ -69,7 +69,7 @@ if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
     export PATH="$HOME/.cargo/bin:$PATH"
 fi
-uv venv --clear
+# uv venv --clear
 source .venv/bin/activate
 uv pip install -r pyproject.toml
 
