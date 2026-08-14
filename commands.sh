c8c8fa15ad86a1b86055c4fe940fa1596fbaf88f
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-08-14 08:56:03 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-~/aiskylimit_nothing/gen_data/deepseek_output/split/Distill_Qwen_32B_generated_outputs_part_3.zip.part_004 +a
+#2 -f-~/aiskylimit_nothing/gen_data/deepseek_output/split/Distill_Qwen_32B_generated_outputs_part_3.zip.part_005 +a
 #talas
 #v1
 
