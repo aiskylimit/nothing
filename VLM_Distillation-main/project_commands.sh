3df93eef299edb9c3661fc62a4950ead72647ebb
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-08-14 23:15:32 +0700

    add code to run VLM Distillation fix venv 3

--- VLM_Distillation-main/project_commands.sh
+++ VLM_Distillation-main/project_commands.sh
@@ -1,9 +1,9 @@
 #conda create -n vlm python=3.11 -y
-conda activate vlm
+#conda activate vlm
 
-#rm -rf ./vlm
-#python -m venv vlm 
-#source vlm/bin/activate
+rm -rf ./vlm
+python -m venv vlm 
+source vlm/bin/activate
 pip install -r requirements.txt
 
 #bash download_datatrain.sh
