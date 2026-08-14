71c48bb9e9dea448751f150aeff0c67074875609
phongdq25 <nothing>
2026-08-14 09:05:33 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#1 +120
-#talas
+#1 +10
+#chi
 #v1
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/segd_distillation/logs/ +a
@@ -42,8 +42,8 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
 
 # cd ./segd_distillation
 # bash ./project_commands.sh
@@ -58,5 +58,5 @@ bash ./project_commands.sh
 # ls -R
 
 
-# cd ./reward-guidance-main
-# bash ./project_command.sh
+cd ./reward-guidance-main
+CUDA_VISIBLE_DEVICES=6,7 bash ./project_command.sh
