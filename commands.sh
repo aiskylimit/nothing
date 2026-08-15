8add83090a60718c196c4c93bce45c34aff51f99
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-08-15 10:02:25 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-~//aiskylimit_nothing/reward-guidance-main/split/export_results.tar.gz.part_02 +a
+#2 -f-~//aiskylimit_nothing/reward-guidance-main/split/export_results.tar.gz.part_03 +a
 #vlm
 #v1
 
