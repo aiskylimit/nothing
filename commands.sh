ff28ab1668aa609e559034f6b97569d94bf16835
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-08-18 20:49:19 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #2 -f-/home/ubuntu/aiskylimit_nothing/segd_distillation/logs/ +a
 #segd
-#v1
+#v2
 
 #2 -f-/home/ubuntu/aiskylimit_nothing/segd_distillation/logs/ +a
 #2 -f-/home/ubuntu/aiskylimit_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
